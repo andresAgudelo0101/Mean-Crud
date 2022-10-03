271fec27f2529da4e22aa11c6903c9043aada8f4
andres agudelo <echeverry11298@gmail.com>
2022-10-03 15:05:26 -0500

    configuracion inicial en el backend y frontend

---
+++ back-end/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = 3001;
+
+app.use(express.json());
+app.use(cors());
+
+app.listen(port,()=>{
+    console.log(`Server runs perfectly on port ${port} `)
+});
